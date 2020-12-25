7e28109ef30b49bb531c561330d24f718c2cbd67
Gursharan Singh <gursharansingh4312@gmail.com>
2020-12-25 00:16:34 -0800

    lambda function changes

--- functions/confessions.js
+++ functions/confessions.js
@@ -1,50 +1,60 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DB_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
 
-const Confession = mongoose.model("Confession", {
-  message: String,
-  date: Date,
-});
+let conn = null;
+const uri = process.env.DB_URL;
 
-exports.handler = async function (event, context, callback) {
+exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  if (conn == null) {
+    conn.mongoose.createConnection(uri, {
+      bufferCommands: false,
+      bufferMaxEntries: 0,
+    });
+    await conn;
+    conn.model(
+      "Confession",
+      new mongoose.Schema({
+        message: String,
+        date: Date,
+      })
+    );
+  }
+  const ConfessionModel = conn.model("Confession");
+  let response;
   if (event.httpMethod === "POST") {
     const { message } = JSON.parse(event.body);
 
-    const newConfession = new Confession({
+    const newConfession = new ConfessionModel({
       message,
       date: new Date(),
     });
 
-    newConfession
-      .save()
-      .then(() => {
-        callback(null, {
-          statusCode: 200,
-          body: "Confession added!!",
-        });
-      })
-      .catch(() => {
-        callback(null, {
-          statusCode: 500,
-          body: "Something went wrong try again!!",
-        });
-      });
+    try {
+      await newConfession.save();
+      response = {
+        statusCode: 200,
+        body: "Confession added!!",
+      };
+    } catch (err) {
+      response = {
+        statusCode: 500,
+        body: "Something went wrong try again!!",
+      };
+    }
   } else if (event.httpMethod === "GET") {
     try {
-      let confessions = await Confession.find({});
-      callback(null, {
+      let confessions = await ConfessionModel.find({});
+      response = {
         statusCode: 200,
         body: JSON.stringify(confessions),
-      });
+      };
     } catch (err) {
-      callback(null, {
+      response = {
         statusCode: 500,
         body: JSON.stringify(err),
-      });
+      };
     }
   }
+  return response;
 };
